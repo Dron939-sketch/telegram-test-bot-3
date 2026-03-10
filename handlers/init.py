e3539e13a203d450cbf2ea94cc91c1239735b114
Dron939-sketch <Dron939@yandex.ru>
2026-03-10 19:00:57 +0300

    Create init.py

---
+++ handlers/init.py
@@ -0,0 +1,12 @@
+"""
+Пакет с обработчиками
+"""
+from .start import *
+from .modes import *
+from .goals import *
+from .reality import *
+from .questions import *
+from .profile import *
+from .routes import *
+from .help import *
+from .admin import *
